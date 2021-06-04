e00a913bb7896e44d6af79a9be0312b243681d9a
anuragpatidar17 <anurag.patidar@yahoo.in>
2021-06-04 15:51:41 +0530

    first

---
+++ server.js
@@ -0,0 +1,64 @@
+
+const fs = require('fs');   //package for txt file
+const pdf = require('pdf-parse');
+ 
+
+
+let dataBuffer = fs.readFileSync('./Anurag_resume.pdf');
+ 
+pdf(dataBuffer).then(function(data) {
+ 
+    // PDF text
+    console.log(data.text); 
+        
+});
+
+
+
+
+
+
+// inputing excel
+//var wb = xlsx.readFile("P2000 Review Sheet - GC.036-March21.xlsx",{type:'binary', cellDates:true, cellNF: false, cellText:false});
+//var wb2 = xlsx.readFile("GC36_P2000_User List_05th_April_2021.xlsx",{type:'binary', cellDates:true, cellNF: false, cellText:false});
+
+// inputing sheets from particular excel
+//var ws = wb.Sheets["OLD User list"];
+//var ws2 = wb2.Sheets["User List"];
+//var ws3 = wb.Sheets["Sheet1"];
+//converting to JSON
+//var data = xlsx.utils.sheet_to_json(ws); //Test Datasets
+//var data2 = xlsx.utils.sheet_to_json(ws2);
+//var data3 = xlsx.utils.sheet_to_json(ws3); // Prod Datasets
+
+// declaring variables
+//var newData = [];
+//var i;
+//var j;//
+//var val1;
+//var val2;
+
+//var oldlist_length = data.length;
+//var newlist_length = data2.length;
+
+
+// Looping through Prod Datasets
+//for(i=0;i<data2.length-1;i++)
+//{
+
+   //newData.push(data2[i].u_name);
+
+    
+
+
+//}
+
+//for(i=1;i<data3.length-1;i++)
+{
+
+   //data3[i].u_name = "anurag";
+
+    
+
+
+}//
